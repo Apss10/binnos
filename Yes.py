11e334aa474294ed9acb96a96ecab9da7f35522b
Apss10 <84327928+Apss10@users.noreply.github.com>
2021-07-01 12:12:26 +0300

    Update Yes.py

--- Yes.py
+++ Yes.py
@@ -2,7 +2,7 @@
 # Python bytecode 2.7
 # Decompiled from: Python 2.7.18 (default, Mar 20 2021, 14:58:25) 
 # [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/
-# Embedded file name: koNtol
+
 import platform
 bit = platform.architecture()[0]
 if bit == '64bit':
