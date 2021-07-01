abf42f4fd33435cf4ea9503a153233dc6cf80d17
Apss10 <84327928+Apss10@users.noreply.github.com>
2021-07-01 11:44:41 +0300

    Add files via upload

---
+++ Yes.py
@@ -0,0 +1,13 @@
+# uncompyle6 version 3.7.4
+# Python bytecode 2.7
+# Decompiled from: Python 2.7.18 (default, Mar 20 2021, 14:58:25) 
+# [GCC 4.2.1 Compatible Android (6454773 based on r365631c2) Clang 9.0.8 (https:/
+# Embedded file name: koNtol
+import platform
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import Jutt
+    Jutt.sec1()
+elif bit == '32bit':
+    import Jutti
+    Jutti.sec1()
